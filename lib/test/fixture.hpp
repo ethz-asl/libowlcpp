7913020ede27a99f864d7c0103e6daa60e907716
mklevin <svarneticist@gmail.com>
2011-06-03 16:30:35 -0500

    make test fixture

--- lib/test/fixture.hpp
+++ lib/test/fixture.hpp
@@ -6,6 +6,8 @@ Distributed under GNU Lesser General Public License; see doc/license.txt.
 #ifndef FIXTURE_HPP_
 #define FIXTURE_HPP_
 #include <string>
+#include "boost/filesystem.hpp"
+#include "boost/test/unit_test_monitor.hpp"
 #include "boost/preprocessor/stringize.hpp"
 #include "owl_cpp/catalog.hpp"
 #include "Kernel.h"
@@ -21,38 +23,32 @@ Distributed under GNU Lesser General Public License; see doc/license.txt.
 #endif
 
 namespace owl_cpp{ namespace test{
+namespace b = boost;
+namespace but = boost::unit_test;
+namespace bfs = boost::filesystem;
 
-/** File names of sample data, catalog
+/** Test fixture for printing exception info
 *******************************************************************************/
-class Sample_data {
-   struct Sample_catalog {
-      Sample_catalog(const std::string& path):cat_(){find_ontologies(cat_, path);}
-      Catalog cat_;
-   private:
-      Sample_catalog();
-   };
-
-public:
-   static const std::string& sample_data_dir() {
-      static const std::string str(BOOST_PP_STRINGIZE(SAMPLE_DATA_DIR));
-      return str;
+struct Fixture {
+   Fixture() {
+      but::unit_test_monitor.register_exception_translator<b::exception>(&translate);
    }
 
-   static const std::string& temp_dir() {
+   static void translate(const b::exception &e) {
+       BOOST_FAIL(b::diagnostic_information(e));
+   }
+
+   static std::string sample_data_path(const std::string& file = "") {
+      static const bfs::path path(BOOST_PP_STRINGIZE(SAMPLE_DATA_DIR));
+      return (path / file).string();
+   }
+
+   static const std::string& temp_path(const std::string& file = "") {
       static const std::string str(BOOST_PP_STRINGIZE(TEMPORARY_DIR));
-      return str;
+      return (bfs::path(str) / file).string();
    }
-
-   static const Catalog& catalog() {return cat_.cat_;}
-
-private:
-   static const Sample_catalog cat_;
-   Triple_store store_;
-   ReasoningKernel kernel_;
 
 };
-
-const Sample_data::Sample_catalog Sample_data::cat_(Sample_data::sample_data_dir());
 
 }//namespace test
 }//namespace owl_cpp
--- lib/test/rdf_parser_01_run.cpp
+++ lib/test/rdf_parser_01_run.cpp
@@ -7,26 +7,27 @@ Distributed under GNU Lesser General Public License; see doc/license.txt.
 #define BOOST_TEST_MODULE rdf_parser_01_run
 #include "boost/test/unit_test.hpp"
 #include <iostream>
-#include "sample_data.hpp"
 #include "owl_cpp/parse_to_triple_store.hpp"
 #include "owl_cpp/triple_store.hpp"
 #include "owl_cpp/print.hpp"
 #include "owl_cpp/query_triples.hpp"
 #include "triple_store_checks.hpp"
+#include "fixture.hpp"
 
 namespace owl_cpp{
 
 namespace test{
+BOOST_FIXTURE_TEST_SUITE(suite, Fixture);
 
 /**
 *******************************************************************************/
 BOOST_AUTO_TEST_CASE( rdf_parser_01_union ) {
 	const char* f_name = "union_01.owl";
-	std::cout << '\n' << f_name << std::endl;
-	const std::string f_path = sample_file_path(f_name);
+//	std::cout << '\n' << f_name << std::endl;
+	const std::string f_path = sample_data_path(f_name);
    Triple_store store;
    load(f_path, store);
-   BOOST_CHECK( check_and_print(store, std::cout) );
+   check(store);
 //   print(store, std::cout);
 }
 
@@ -34,20 +35,15 @@ BOOST_AUTO_TEST_CASE( rdf_parser_01_union ) {
 *******************************************************************************/
 BOOST_AUTO_TEST_CASE( rdf_parser_01_trans_property ) {
 	const char* f_name = "transitive_property.owl";
-	std::cout << '\n' << f_name << std::endl;
-	const std::string f_path = sample_file_path(f_name);
+//	std::cout << '\n' << f_name << std::endl;
+	const std::string f_path = sample_data_path(f_name);
    Triple_store store;
-   try{
-      load(f_path, store);
-   } catch(...) {
-      std::cerr << b::current_exception_diagnostic_information() << std::endl;
-      throw;
-   }
+   load(f_path, store);
 
-   BOOST_CHECK( check_and_print(store, std::cout) );
-
+   check(store);
 //   print(store, std::cout);
 }
 
+BOOST_AUTO_TEST_SUITE_END();
 }//namespace test
 }//namespace owl_cpp
