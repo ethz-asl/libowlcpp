55769fc841820222595b9360ddd185e2f20c6236
svarneticist <svarneticist@gmail.com>
2011-12-22 14:39:08 -0600

    test Python API

---
+++ binding/python/test/triple_store.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+r'''
+>>> import owlcpp.rdf
+>>> ts = owlcpp.rdf.Triple_store()
+>>> ts.n_triples()
+0
+'''
+
+def run(args = None):
+    if args is not None:
+        import sys
+        sys.argv = args
+    import doctest
+    return doctest.testmod(verbose=True)
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(run()[0])
