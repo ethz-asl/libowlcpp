7c12d0f7dca51e80ddc95b5fb5dcd4a829b80a1f
mlevin <mlevin@svarnetics.org>
2012-01-11 16:55:31 -0600

    fix include

--- lib/rdf/test/node01_run.cpp
+++ lib/rdf/test/node01_run.cpp
@@ -4,11 +4,11 @@ Distributed under GNU General Public License; see doc/license.txt.
 @date 2012 @author Mikhail K Levin
 *******************************************************************************/
 #define BOOST_TEST_MODULE node01_run
+#include <iostream>
 #include "boost/test/unit_test.hpp"
 #include "test/exception_translator.hpp"
 #include "owlcpp/node.hpp"
 #include "type_vector.hpp"
-
 namespace owlcpp{ namespace test{
 
 BOOST_GLOBAL_FIXTURE( Exception_translator );
