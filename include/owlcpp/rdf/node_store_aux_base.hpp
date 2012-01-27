901862ececc0d202c380b466da02ff75688e1fc4
mlevin <mlevin@svarnetics.org>
2012-01-26 19:39:34 -0600

    insertion of blank and literal nodes (unfinished)

--- include/owlcpp/rdf/node_store_aux_base.hpp
+++ include/owlcpp/rdf/node_store_aux_base.hpp
@@ -6,6 +6,7 @@ part of owlcpp project.
 #ifndef NODE_STORE_AUX_BASE_HPP_
 #define NODE_STORE_AUX_BASE_HPP_
 #include "owlcpp/rdf/node_map.hpp"
+#include "owlcpp/terms/node_tags_system.hpp"
 
 namespace owlcpp{
 
@@ -23,14 +24,22 @@ template<class T> struct Node_store_aux_base {
             std::string const& val,
             std::string const& type_iri = "",
             std::string const& lang = ""
-   ); //TODO:
+   ) {
+      T& self = static_cast<T&>(*this);
+      const Node_id nid = self.nodes().insert( Node(terms::N_empty::id(), val));
+      //TODO: store type_iri and language
+      return nid;
+   }
 
    /**@brief if not already present, store blank node
     @param name node name;
     name is assumed to be unique across all documents stored in Triple_store
     @return node ID
    */
-   Node_id insert_blank_node(std::string const& name); //TODO:
+   Node_id insert_blank_node(std::string const& name) {
+      T& self = static_cast<T&>(*this);
+      return self.nodes().insert( Node(terms::N_blank::id(), name));
+   }
 
 };
 
