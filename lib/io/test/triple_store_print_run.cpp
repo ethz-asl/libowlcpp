61d414a9c88991cbdb377768ac30133e858fe534
mlevin <mlevin@svarnetics.org>
2012-03-11 21:19:46 -0500

    pretty-print triples for sample OWL files

---
+++ lib/io/test/triple_store_print_run.cpp
@@ -0,0 +1,52 @@
+/** @file "/owlcpp/lib/io/test/triple_store_print_run.cpp" 
+part of owlcpp project.
+@n @n Distributed under the Boost Software License, Version 1.0; see doc/license.txt.
+@n Copyright Mikhail K Levin 2012
+*******************************************************************************/
+#define BOOST_TEST_MODULE triple_store_print_run
+#include "boost/test/unit_test.hpp"
+#include "test/exception_fixture.hpp"
+#include "boost/foreach.hpp"
+#include "boost/lexical_cast.hpp"
+#include "test/sample_data.hpp"
+#include "owlcpp/rdf/triple_store.hpp"
+#include "owlcpp/io/input.hpp"
+#include "owlcpp/terms/node_tags_system.hpp"
+
+namespace owlcpp{ namespace test{
+
+BOOST_GLOBAL_FIXTURE( Exception_fixture );
+
+std::string short_str(const Node_id nid, Triple_store const& ts) {
+   Node const& node = ts[nid];
+   const Ns_id nsid = node.ns_id();
+   const std::string val = node.value_str();
+   if( nsid == owlcpp::terms::N_blank::id() ) return "_:" + val;
+   if( nsid == owlcpp::terms::N_empty::id() ) return "\"" + val + "\"";
+   if( val.empty() ) return ts[nsid];
+   const std::string pref = ts.iris().prefix(nsid);
+   return (
+            pref.empty() ? "ns" + boost::lexical_cast<std::string>(nsid()) : pref
+   ) + ":" + node.value_str();
+}
+
+/**
+*******************************************************************************/
+BOOST_AUTO_TEST_CASE( case01 ) {
+   BOOST_FOREACH(Sample_info const& si, sample_files()) {
+      std::cout << si.path << '\n';
+      Triple_store ts(Node_map_std::get(Nodes_owl()));
+      load_file(si.path, ts);
+      BOOST_FOREACH(Triple const& t, ts.triples()) {
+         std::cout
+         << short_str(t.subject(), ts) << ' '
+         << short_str(t.predicate(), ts) << ' '
+         << short_str(t.object(), ts) << '\n'
+         ;
+      }
+      std::cout << '\n';
+   }
+}
+
+}//namespace test
+}//namespace owlcpp
