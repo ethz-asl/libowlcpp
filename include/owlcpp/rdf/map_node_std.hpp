71afc8b5d5ccc45021cd052cb4bf7ceda9be47e3
mlevin <mlevin@svarnetics.org>
2012-06-24 23:08:31 -0500

    fix standard node map

--- include/owlcpp/rdf/map_node_std.hpp
+++ include/owlcpp/rdf/map_node_std.hpp
@@ -11,7 +11,7 @@ part of owlcpp project.
 #include "boost/utility.hpp"
 
 #include "owlcpp/rdf/detail/map_ns_base.hpp"
-#include "owlcpp/rdf/detail/map_node_base.hpp"
+#include "owlcpp/rdf/detail/map_node_iri_base.hpp"
 #include "owlcpp/terms/node_tags_system.hpp"
 
 namespace owlcpp{
@@ -21,7 +21,7 @@ namespace owlcpp{
 *******************************************************************************/
 class Map_node_std : boost::noncopyable {
    typedef detail::Map_ns_base map_ns;
-   typedef detail::Map_node_base map_node;
+   typedef detail::Map_node_iri_base map_node;
 
    template<class Inserter> explicit Map_node_std(Inserter const& ins)
    : ns_(Ns_id(0)), ns_id_next_(0), node_(Node_id(0)), node_id_next_(0)
@@ -72,20 +72,34 @@ public:
    template<class NTag> void insert_node_tag(NTag const&) {
       typedef typename NTag::ns_type ns_type;
       insert_ns_tag(ns_type());
-      node_.insert_iri(NTag::id(), ns_type::id(), NTag::name());
+      node_.insert(NTag::id(), ns_type::id(), NTag::name());
+   }
+
+   /**
+    @param nsid namespace IRI ID
+    @return true if nodes should not be added to this namespace by user;
+    e.g., in case of RDF or OWL namespaces.
+    That is, all nodes from this namespace should reside in this map.
+   */
+   bool is_standard(const Ns_id nsid) const {
+      return
+               nsid != terms::N_empty::id() &&
+               nsid != terms::N_blank::id() &&
+               nsid < ns_id_next_
+               ;
    }
 
    Ns_id ns_id_next() const {return ns_id_next_;}
    Ns_id const* find_iri(std::string const& iri) const {return ns_.find_iri(iri);}
    Ns_id const* find_prefix(std::string const& pref) const {return ns_.find_prefix(pref);}
-   std::string operator[](const Ns_id iid) const {return ns_[iid];}
-   std::string at(const Ns_id iid) const {return ns_.at(iid);}
-   std::string prefix(const Ns_id iid) const {return ns_.prefix(iid);}
+   std::string operator[](const Ns_id nsid) const {return ns_[nsid];}
+   std::string at(const Ns_id nsid) const {return ns_.at(nsid);}
+   std::string prefix(const Ns_id nsid) const {return ns_.prefix(nsid);}
 
    Node_id node_id_next() const {return node_id_next_;}
-   Node const& operator[](const Node_id nid) const {return node_[nid];}
-   Node const& at(const Node_id nid) const {return node_.at(nid);}
-   Node_id const* find(Node const& node) const {return node_.find(node);}
+   Node_iri const& operator[](const Node_id nid) const {return node_[nid];}
+   Node_iri const& at(const Node_id nid) const {return node_.at(nid);}
+   Node_id const* find(Node_iri const& node) const {return node_.find(node);}
 
 private:
    map_ns ns_;
