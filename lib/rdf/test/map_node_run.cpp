be9c8f6de8dd3a1dc065873b0312134666d467aa
mlevin <mlevin@svarnetics.org>
2012-10-01 18:29:43 -0500

    remove old files

--- lib/rdf/test/map_node_run.cpp
+++ lib/rdf/test/map_node_run.cpp
@@ -1,9 +1,9 @@
-/** @file "/owlcpp/lib/rdf/test/map_node_base_run.cpp" 
+/** @file "/owlcpp/lib/rdf/test/map_node_run.cpp"
 part of owlcpp project.
 @n @n Distributed under the Boost Software License, Version 1.0; see doc/license.txt.
 @n Copyright Mikhail K Levin 2012
 *******************************************************************************/
-#define BOOST_TEST_MODULE map_node_base_run
+#define BOOST_TEST_MODULE map_node_run
 #include "boost/test/unit_test.hpp"
 #include "test/exception_fixture.hpp"
 #include "owlcpp/rdf/map_node.hpp"
@@ -28,7 +28,7 @@ BOOST_AUTO_TEST_CASE( case01 ) {
    const Node_id id2 = mnb.insert_blank(42, Doc_id(5));
    BOOST_CHECK( mnb.have(id2) );
 
-   const Node_id id3 = mnb.insert_literal("blah", Node_id(5), "jp");
+   const Node_id id3 = mnb.insert_literal("blah", Node_id(5), "en");
    BOOST_CHECK( mnb.have(id3) );
 
    BOOST_CHECK_NE(id1, id2);
@@ -37,7 +37,7 @@ BOOST_AUTO_TEST_CASE( case01 ) {
    std::auto_ptr<Node> p1 = mnb.remove(id1);
    BOOST_CHECK( ! mnb.find(*p1) );
 
-   const Node_id id4 = mnb.insert_literal("blahblah", Node_id(5), "jp");
+   const Node_id id4 = mnb.insert_literal("blahblah", Node_id(5), "fr");
    BOOST_CHECK( mnb.have(id4) );
    BOOST_CHECK_EQUAL( id1, id4 );
 
