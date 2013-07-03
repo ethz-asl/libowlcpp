eb4aaf001d49a40252a61a4b9a3cf0a6ffcc9324
Mikhail Levin <mlevin@svarnetics.org>
2013-07-03 17:04:11 -0500

    triple fragment set

--- include/owlcpp/rdf/detail/fragment_set.hpp
+++ include/owlcpp/rdf/detail/fragment_set.hpp
@@ -9,7 +9,8 @@ part of owlcpp2 project.
 #include <algorithm>
 #include <functional>
 #include "boost/fusion/container/vector.hpp"
-#include "boost/fusion/sequence/comparison.hpp"
+#include "boost/fusion/sequence/comparison/less.hpp"
+#include "boost/fusion/sequence/comparison/equal_to.hpp"
 #include "boost/iterator/filter_iterator.hpp"
 #include "boost/range.hpp"
 #include "boost/range/algorithm/equal_range.hpp"
@@ -17,6 +18,99 @@ part of owlcpp2 project.
 #include "owlcpp/rdf/exception.hpp"
 
 namespace owlcpp{ namespace map_triple_detail{
+
+template<
+   class T0, class T1, class T2,
+   class U0, class U1, class U2
+> class Equal
+         : public std::unary_function<boost::fusion::vector3<T0,T1,T2>, bool> {
+   typedef boost::fusion::vector3<U0,U1,U2> stor_t;
+   typedef boost::fusion::vector3<T0,T1,T2> fragment;
+
+public:
+   explicit Equal(const U0 u0, const U1 u1, const U2 u2) : e_(u0,u1,u2) {}
+   bool operator()(fragment const& f) const {return f == e_;}
+
+private:
+   stor_t e_;
+};
+
+template<
+   class T0, class T1, class T2,
+   class U0, class U1, class U2
+> class Find {
+   typedef boost::fusion::vector3<T0,T1,T2> fragment;
+   typedef std::vector<fragment> vector;
+   typedef Equal<T0,T1,T2,U0,U1,U2> equal;
+public:
+   typedef boost::filter_iterator<equal, typename vector::const_iterator> iterator;
+   typedef boost::iterator_range<iterator> range;
+   static range find(vector const& v, const U0 u0, const U1 u1, const U2 u2) {
+      const equal eq(u0,u1,u2);
+      return range(
+               iterator(eq, v.begin(), v.end()),
+               iterator(eq, v.end(), v.end())
+      );
+   }
+};
+
+template<
+   class T0, class T1, class T2
+> class Find<T0,T1,T2,T0,T1,T2> {
+   typedef boost::fusion::vector3<T0,T1,T2> fragment;
+   typedef std::vector<fragment> vector;
+public:
+   typedef typename vector::const_iterator iterator;
+   typedef boost::iterator_range<iterator> range;
+   static range find(vector const& v, const T0 t0, const T1 t1, const T2 t2) {
+      return boost::equal_range(v, fragment(t0,t1,t2));
+   }
+};
+
+template<
+   class T0, class T1, class T2
+> class Find<T0,T1,T2,T0,T1,any> {
+   typedef boost::fusion::vector3<T0,T1,T2> fragment;
+   typedef std::vector<fragment> vector;
+public:
+   typedef typename vector::const_iterator iterator;
+   typedef boost::iterator_range<iterator> range;
+   static range find(vector const& v, const T0 t0, const T1 t1, const any) {
+      return boost::equal_range(
+               v,
+               boost::fusion::vector3<T0,T1,any>(t0,t1,any())
+      );
+   }
+};
+
+template<
+   class T0, class T1, class T2
+> class Find<T0,T1,T2,T0,any,any> {
+   typedef boost::fusion::vector3<T0,T1,T2> fragment;
+   typedef std::vector<fragment> vector;
+public:
+   typedef typename vector::const_iterator iterator;
+   typedef boost::iterator_range<iterator> range;
+   static range find(vector const& v, const T0 t0, const any, const any) {
+      return boost::equal_range(
+               v,
+               boost::fusion::vector3<T0,any,any>(t0, any(), any())
+      );
+   }
+};
+
+template<
+   class T0, class T1, class T2
+> class Find<T0,T1,T2,any,any,any> {
+   typedef boost::fusion::vector3<T0,T1,T2> fragment;
+   typedef std::vector<fragment> vector;
+public:
+   typedef typename vector::const_iterator iterator;
+   typedef boost::iterator_range<iterator> range;
+   static range find(vector const& v, const any, const any, const any) {
+      return v;
+   }
+};
 
 /**@brief 
 *******************************************************************************/
@@ -30,12 +124,7 @@ public:
    typedef boost::iterator_range<const_iterator> const_range;
 
    template<class U0, class U1, class U2> struct result {
-      typedef boost::filter_iterator<
-               std::equal_to<value_type, boost::fusion::vector3<U0,U1,U2> >,
-               const_iterator
-               >
-      iterator;
-      typedef boost::iterator_range<iterator> range;
+      typedef typename Find<T0,T1,T2,U0,U1,U2>::range type;
    };
 
    const_iterator begin() const {return v_.begin();}
@@ -59,17 +148,9 @@ public:
    }
 
    template<class U0, class U1, class U2>
-   typename result<U0,U1,U2>::range
-   find(boost::fusion::vector3<U0,U1,U2> const& t) const {
-      //todo:
-   }
-
-   const_range find(value_type const& t) const {
-      return boost::equal_range(v_, t);
-   }
-
-   const_range find(boost::fusion::vector3<any,any,any> const& ) const {
-      return v_;
+   typename Find<T0,T1,T2,U0,U1,U2>::range
+   find(const U0 u0, const U1 u1, const U2 u2) const {
+      return Find<T0,T1,T2,U0,U1,U2>::find(v_, u0, u1, u2);
    }
 
 private:
--- lib/rdf/test/triple_index_run.cpp
+++ lib/rdf/test/triple_index_run.cpp
@@ -18,20 +18,30 @@ BOOST_GLOBAL_FIXTURE( Exception_fixture );
 
 namespace m = map_triple_detail;
 
-/**
+/** Test fragment set
 *******************************************************************************/
 BOOST_AUTO_TEST_CASE( case01 ) {
    typedef m::Fragment_set<Doc_id,Node_id,Node_id> fs_t;
    fs_t fs;
    typedef fs_t::value_type val_t;
    fs.insert( val_t(Doc_id(1), Node_id(1), Node_id(2)) );
+   fs.insert( val_t(Doc_id(2), Node_id(1), Node_id(1)) );
+   fs.insert( val_t(Doc_id(2), Node_id(2), Node_id(1)) );
+   fs.insert( val_t(Doc_id(2), Node_id(1), Node_id(2)) );
    fs.insert( val_t(Doc_id(1), Node_id(1), Node_id(1)) );
    fs.insert( val_t(Doc_id(1), Node_id(1), Node_id(1)) );
    fs.insert( val_t(Doc_id(1), Node_id(2), Node_id(1)) );
-   BOOST_CHECK_EQUAL(fs.size(), 3U);
+   BOOST_CHECK_EQUAL(fs.size(), 6U);
    typedef fs_t::const_range range_t;
-   range_t r = fs.find(boost::fusion::make_vector(any(), Node_id(1), any()));
-   BOOST_CHECK_EQUAL(r.size(), 2U);
+   range_t r1 = fs.find(any(), any(), any());
+   BOOST_CHECK_EQUAL(r1.size(), 6U);
+   range_t r2 = fs.find(Doc_id(1), any(), any());
+   BOOST_CHECK_EQUAL(r2.size(), 3U);
+   range_t r3 = fs.find(Doc_id(1), Node_id(2), any());
+   BOOST_CHECK_EQUAL(r3.size(), 1U);
+
+   fs_t::result<Doc_id,any,Node_id>::type r4 = fs.find(Doc_id(2), any(), Node_id(1));
+   BOOST_CHECK_EQUAL(distance(r4), 2);
 }
 
 }//namespace test
