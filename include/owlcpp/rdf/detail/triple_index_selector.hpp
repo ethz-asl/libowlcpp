8faf5972fd7825c47883ba46a9a6536a506c6e10
Mikhail Levin <mlevin@svarnetics.org>
2013-08-02 15:14:35 -0500

    change index scoring

--- include/owlcpp/rdf/detail/triple_index_selector.hpp
+++ include/owlcpp/rdf/detail/triple_index_selector.hpp
@@ -6,12 +6,11 @@ part of owlcpp2 project.
 #ifndef TRIPLE_INDEX_SELECTOR_HPP_
 #define TRIPLE_INDEX_SELECTOR_HPP_
 #include "boost/mpl/fold.hpp"
-#include "boost/mpl/transform.hpp"
-#include "boost/mpl/fold.hpp"
+#include "boost/mpl/front.hpp"
+#include "boost/mpl/at.hpp"
 #include "boost/mpl/if.hpp"
 #include "boost/mpl/less.hpp"
 #include "boost/mpl/max_element.hpp"
-#include "boost/mpl/transform_view.hpp"
 #include "boost/mpl/plus.hpp"
 #include "boost/mpl/push_back.hpp"
 #include "boost/mpl/vector.hpp"
@@ -23,53 +22,89 @@ part of owlcpp2 project.
 namespace owlcpp{ namespace map_triple_detail{
 
 /**@brief Estimate of how well a given triple index would perform a search
-@tparam Tags vector of integers showing how triples are sorted by index
-,e.g., @code mpl::vector4<Obj_tag, Doc_tag, Subj_tag, Pred_tag> @endcode
-@tparam QArgs sequence of query element types, e.g.,
-@code mpl::vector4<any, any, Node_id, Doc_id> @endcode
+@tparam Tags sequence of integer types showing how triples are sorted by index
+,e.g., for index that sorts triples by object, by docID, by subject, and then
+by predicate, the sequence is
+@code mpl::vector4<Obj_tag, Doc_tag, Subj_tag, Pred_tag> @endcode
+@tparam QBSig boolean signature of a query, a sequence of 4 booleans indicating,
+for each triple element, whether explicit match was requested,
+e.g., for a query @code any, any, Node_id, Doc_id @endcode
+boolean signature is @code mpl::vector4_c<bool,0,0,1,1>@endcode
 @tparam Prefs sequence of relative search efficiencies for each triple element,
-e.g., query<any, any, Node_id, Doc_id> is faster using
+e.g., query<any, any, Node_id, Doc_id> is expected to be faster using
 index<Obj_tag, Doc_tag, Subj_tag, Pred_tag> than using
-index<Doc_tag, Obj_tag, Subj_tag, Pred_tag>
+index<Doc_tag, Obj_tag, Subj_tag, Pred_tag>,
+because in an ontology there are usually more different object nodes than
+different documents and grouping triples by object creates smaller groups
+and results in a more efficient search.
 *******************************************************************************/
 template<
    class Tags,
-   class QArgs,
-   class Prefs = boost::mpl::vector4_c<int,4,2,3,1>
+   class QBSig
 > class Search_efficiency {
-   typedef boost::mpl::vector4_c<int,0, 1, 2, 3> count;
-   typedef boost::mpl::vector4_c<int,100, 10, 1, 0> scores;
-public:
-   typedef typename boost::mpl::fold<
-            count,
-            boost::mpl::int_<0>,
-            boost::mpl::if_<
-               boost::is_same<
-                  boost::mpl::at<Triple, boost::mpl::at<Tags, boost::mpl::_2> >,
-                  boost::mpl::at<QArgs, boost::mpl::at<Tags, boost::mpl::_2> >
-                  >,
-               boost::mpl::plus<
-                  boost::mpl::_1,
-                  boost::mpl::times<
-                     boost::mpl::at<scores, boost::mpl::_2>,
-                     boost::mpl::at<Prefs, boost::mpl::at<Tags, boost::mpl::_2> >
-                  >
-               >,
-               boost::mpl::_1
+
+   typedef boost::mpl::int_<100> key1_value;
+   typedef boost::mpl::int_<10>  key2_value;
+   typedef boost::mpl::int_<1>   key3_value;
+
+   /** relative diversity of elements in each triple position */
+   typedef boost::mpl::vector4_c<int,4, 2, 3, 1> diversity;
+
+   /**  */
+   typedef typename boost::mpl::if_<
+            typename boost::mpl::at<
+               QBSig,
+               typename boost::mpl::front<Tags>::type
+            >::type,
+            boost::mpl::times<
+               key1_value,
+               typename boost::mpl::at<
+                  diversity,
+                  typename boost::mpl::front<Tags>::type
+               >::type
+            >,
+            boost::mpl::negate<
+               typename boost::mpl::at<
+                  diversity,
+                  typename boost::mpl::front<Tags>::type
+               >::type
             >
-            >::type type;
+   >::type main_key_score;
+
+   typedef typename boost::mpl::if_<
+            typename boost::mpl::at<
+               QBSig,
+               typename boost::mpl::at_c<Tags,1>::type
+            >::type,
+            boost::mpl::plus<
+               key2_value,
+               typename boost::mpl::if_<
+                  typename boost::mpl::at<
+                     QBSig,
+                     typename boost::mpl::at_c<Tags,2>::type
+                  >::type,
+                  key3_value,
+                  boost::mpl::int_<0>
+               >::type
+            >,
+            boost::mpl::int_<0>
+   >::type sec_keys_score;
+
+
+public:
+   typedef typename boost::mpl::plus<main_key_score, sec_keys_score>::type type;
    static const int value = type::value;
 };
 
 template<
    class Indexes,
-   class QArgs
+   class QBSig
 > struct Index_selector {
 
    template<class I1, class I2> struct less
             : boost::mpl::less<
-              typename Search_efficiency<typename I1::sort_order, QArgs>::type,
-              typename Search_efficiency<typename I2::sort_order, QArgs>::type
+              typename Search_efficiency<typename I1::sort_order, QBSig>::type,
+              typename Search_efficiency<typename I2::sort_order, QBSig>::type
               >
             {};
 
